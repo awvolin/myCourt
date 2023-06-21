50ad5151addcb7b5b9660ae45038d5795dcc7058
awvolin <alex.volin123@gmail.com>
2023-06-21 14:06:43 -0400

    Initial Commit

---
+++ myCourt/myCourtApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCourtApp.swift
+//  myCourt
+//
+//  Created by Alex Volin on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct myCourtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
