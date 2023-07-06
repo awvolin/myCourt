2ba4d67234eaa39aff348dbb231ce43e026787ec
awvolin <alex.volin123@gmail.com>
2023-07-06 13:41:17 -0400

    GitHub linked

--- myCourt/ContentView.swift
+++ myCourt/ContentView.swift
@@ -6,21 +6,152 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
 
 struct ContentView: View {
+    @State private var showingLoginView = false
+    @State var vm = ViewModel()
+    @State private var loggedIn = false
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            //Initial splashscreen
+            if(!showingLoginView) {
+                GeometryReader{ geo in
+                    ZStack (alignment: .top) {
+                        Image("Wallpaper")
+                            .resizable()
+                            .aspectRatio(geo.size, contentMode: .fill)
+                            .edgesIgnoringSafeArea(.all)
+                        VStack {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame( height: 140)
+                                .foregroundColor(Color.white)
+                                .padding(25)
+                            Button {
+                                showingLoginView = true
+                            } label: {
+                                Text("I got next.")
+                            }
+                            .padding(10)
+                            .background(.orange)
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .cornerRadius(10)
+                        }
+                    }
+                }
+            }
+            else {
+                
+                //Login Page
+                if !loggedIn {
+                    GeometryReader{ geo in
+                        ZStack (alignment: .top) {
+                            Color(hue: 0, saturation: 0, brightness: 0.77)
+                                .aspectRatio(geo.size, contentMode: .fill)
+                                .edgesIgnoringSafeArea(.all)
+                            
+                            VStack {
+                                Image("logo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame( height: 140)
+                                    .foregroundColor(Color.white)
+                                    .padding(25)
+                                TextField("Username", text: $vm.username)
+                                    .textFieldStyle(.roundedBorder)
+                                    .textInputAutocapitalization(.never)
+                                SecureField("Password", text: $vm.password)
+                                    .textFieldStyle(.roundedBorder)
+                                    .textInputAutocapitalization(.never)
+                                    .privacySensitive()
+                                HStack {
+                                    Spacer()
+                                    Button("Forgot password?", action: vm.logPressed)
+                                    Spacer()
+                                    Button("Log in", action: {
+                                        vm.authenticate()
+                                        if(vm.authenticated) {
+                                            loggedIn.toggle()
+                                        }
+                                    })
+                                    Spacer()
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                    
+                }
+                
+                //Logged in - launch loggedInView
+                else {
+                    LoggedInView(loggedIn: $loggedIn, username: vm.username, logOutAction: vm.logOut)
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
+struct LoggedInView: View {
+    @Binding var loggedIn: Bool
+    var username: String
+    var logOutAction: () -> Void
+    
+    @StateObject private var mapAPI = MapAPI()
+    @State private var text = ""
+    
+    @State private var showingPopover = false
+
+    var body: some View {
+        VStack(spacing: 20) {
+            TextField("Enter an address", text: $text)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+            
+            Button("Find address") {
+                mapAPI.getLocation(address: text, delta: 0.5)
+            }
+            
+        
+            Button("Log out", action: {
+                logOutAction()
+                loggedIn = false
+            })
+            
+            
+            Map(coordinateRegion: $mapAPI.region, annotationItems: mapAPI.locations) {
+                location in
+                MapMarker(coordinate: location.coordinate, tint: .blue)
+            }
+            .ignoresSafeArea()
+            
+            Button("Show Menu") {
+                        showingPopover = true
+                    }
+            .popover(isPresented: $showingPopover) {
+                Text("Your content here")
+                    .font(.headline)
+                    .padding()
+                
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
