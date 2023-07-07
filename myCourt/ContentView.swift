62d66ae161cb62da282134285050b42029c4d281
awvolin <alex.volin123@gmail.com>
2023-07-06 22:34:27 -0400

    test

--- myCourt/ContentView.swift
+++ myCourt/ContentView.swift
@@ -147,11 +147,10 @@ struct LoggedInView: View {
         }
     }
 }
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-
+    }}
+ 
