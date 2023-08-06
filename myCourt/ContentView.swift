2913054388373119fcd96de80ba58d52a50e2d67
awvolin <alex.volin123@gmail.com>
2023-08-06 17:32:39 -0400

    fixed matched geometry

--- myCourt/ContentView.swift
+++ myCourt/ContentView.swift
@@ -111,129 +111,127 @@ struct LoggedInView: View {
     @Binding var loggedIn: Bool
     var username: String
     var logOutAction: () -> Void
-
+    
     @State private var courtName: String = ""
     @StateObject private var model = CourtViewModel()
     @Namespace var namespace
     @State private var selectedCourt: Court?
-
+    
     var body: some View {
-        ZStack {
-            Color.white.edgesIgnoringSafeArea(.all) // White background
-
-            if selectedCourt == nil {
-                VStack(spacing: 20) {
-                    TextField("Enter Court", text: $courtName)
-                        .textFieldStyle(.roundedBorder)
-                        .padding()
-                        .onSubmit {
+       
+        if selectedCourt == nil {
+            VStack(spacing: 20) {
+                TextField("Enter Court", text: $courtName)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    .onSubmit {
+                        Task {
+                            do {
+                                let court = Court(name: courtName)
+                                try await model.addCourt(court: court)
+                            } catch {
+                                print("Failed to add court: \(error)")
+                            }
+                        }
+                    }
+                
+                ScrollView {
+                    ForEach(model.courts, id: \.id) { court in
+                        VStack (alignment: .leading, spacing: 12) {
+                            image(from: court.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(height: 120)
+                                .clipped()
+                                .matchedGeometryEffect(id: "image\(String(describing: court.id))", in: namespace)
+                            
+                            VStack(alignment: .leading) {
+                                Text(court.name)
+                                    .font(.largeTitle.weight(.bold))
+                                    .matchedGeometryEffect(id: "title\(String(describing: court.id))", in: namespace)
+                                
+                                if let courtDescription = court.description {
+                                    Text(courtDescription)
+                                        .font(.footnote)
+                                        .foregroundColor(.gray)
+                                        .matchedGeometryEffect(id: "subtitle\(String(describing: court.id))", in: namespace)
+                                }
+                            }
+                            .padding()
+                        }
+                        .background(Color.white
+                            .matchedGeometryEffect(id: "background\(String(describing: court.id))", in: namespace))
+                        .cornerRadius(15)
+                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                        .padding(10)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                                selectedCourt = court
+                            }
+                        }
+                    }
+                }
+                
+                Button("Log out", action: {
+                    logOutAction()
+                    loggedIn = false
+                })
+            }
+            .onAppear {
                             Task {
                                 do {
-                                    let court = Court(name: courtName)
-                                    try await model.addCourt(court: court)
+                                    try await model.getCourts()
                                 } catch {
-                                    print("Failed to add court: \(error)")
+                                    print("Error fetching courts: \(error)")
                                 }
                             }
                         }
-
-                    ScrollView {
-                        ForEach(model.courts, id: \.id) { court in
-                            VStack (alignment: .leading, spacing: 12) {
-                                image(from: court.image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(height: 120)
-                                    .clipped()
-                                    .matchedGeometryEffect(id: "image\(String(describing: court.id))", in: namespace)
-                                
-                                VStack(alignment: .leading) {
-                                    Text(court.name)
-                                        .font(.largeTitle.weight(.bold))
-                                        .matchedGeometryEffect(id: "title\(String(describing: court.id))", in: namespace)
-
-                                    if let courtDescription = court.description {
-                                        Text(courtDescription)
-                                            .font(.footnote)
-                                            .foregroundColor(.gray)
-                                            .matchedGeometryEffect(id: "subtitle\(String(describing: court.id))", in: namespace)
-                                    }
-                                }
-                                .padding()
-                            }
-                            .background(Color.white
-                                .matchedGeometryEffect(id: "background\(String(describing: court.id))", in: namespace))
-                            .cornerRadius(15)
-                            .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
-                            .padding(10)
-                            .onTapGesture {
-                                withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
-                                    selectedCourt = court
-                                }
-                            }
-                        }
-                    }
-
-                    Button("Log out", action: {
-                        logOutAction()
-                        loggedIn = false
-                    })
-                }
-            } else {
-                VStack {
-                    image(from: selectedCourt?.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(height: 300)
-                        .clipped()
-                        .matchedGeometryEffect(id: "image\(String(describing: selectedCourt!.id))", in: namespace)
+        } else {
+            VStack {
+                image(from: selectedCourt?.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 300)
+                    .clipped()
+                    .matchedGeometryEffect(id: "image\(String(describing: selectedCourt!.id))", in: namespace)
+                
+                VStack(alignment: .leading) {
+                    Text(selectedCourt!.name)
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title\(String(describing: selectedCourt!.id))", in: namespace)
                     
-                    VStack(alignment: .leading) {
-                        Text(selectedCourt!.name)
-                            .font(.largeTitle.weight(.bold))
-                            .matchedGeometryEffect(id: "title\(String(describing: selectedCourt!.id))", in: namespace)
-
-                        if let selectedCourtDescription = selectedCourt?.description {
-                            Text(selectedCourtDescription)
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                                .matchedGeometryEffect(id: "subtitle\(String(describing: selectedCourt!.id))", in: namespace)
-                        }
-                    }
-                    .padding()
-                }
-                .background(Color.white
-                    .matchedGeometryEffect(id: "background\(String(describing: selectedCourt!.id))", in: namespace)
-                )
-                .cornerRadius(15)
-                .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 10)
-                .padding(50)
-                .onTapGesture {
-                    withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
-                        selectedCourt = nil
+                    if let selectedCourtDescription = selectedCourt?.description {
+                        Text(selectedCourtDescription)
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                            .matchedGeometryEffect(id: "subtitle\(String(describing: selectedCourt!.id))", in: namespace)
                     }
                 }
+                .padding()
             }
-        }
-        .onAppear {
-            Task {
-                do {
-                    try await model.getCourts()
-                }
-                catch {
-                    print("Error fetching courts: \(error)")
+            .background(Color.white
+                .matchedGeometryEffect(id: "background\(String(describing: selectedCourt!.id))", in: namespace)
+            )
+            .cornerRadius(15)
+            .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 10)
+            .padding(50)
+            .onTapGesture {
+                withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                    selectedCourt = nil
                 }
             }
         }
     }
-
-    func image(from asset: CKAsset?) -> Image {
-        guard let asset = asset, let data = try? Data(contentsOf: asset.fileURL!), let uiImage = UIImage(data: data) else {
-            return Image("defaultImageName") // Replace with your placeholder image name if the CKAsset is nil or invalid
-        }
-        return Image(uiImage: uiImage)
-    }
+    
 }
+
+func image(from asset: CKAsset?) -> Image {
+    guard let asset = asset, let data = try? Data(contentsOf: asset.fileURL!), let uiImage = UIImage(data: data) else {
+        return Image("defaultImageName") // Replace with your placeholder image name if the CKAsset is nil or invalid
+    }
+    return Image(uiImage: uiImage)
+}
+
 
 
 
